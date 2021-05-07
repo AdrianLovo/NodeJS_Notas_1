55d498c14506b3d98310450f3fc3a30036a43ae6
AdrianLovo <adrianhumbertocastillo@gmail.com>
2021-05-07 10:32:54 -0600

    Rutas y capa de red

--- server.js
+++ server.js
@@ -10,25 +10,7 @@ app.use(express.urlencoded({extended: false}));
 router(app);
 
 
-
-
-// router.delete('/message', function(req, res){
-//     console.log(req.query);
-//     console.log(req.body);
-//     res.send('Mensaje ' + req.body.text + ' añadido correctamente');
-
-//     RESPUESTAS
-//     res.send('');                //Respuesta vacia
-//     res.status(201).send();     //Respuesta con un estatus
-//     res.status(201).send({          //Respuesta con estatus y datos
-//         error: '',
-//         body: 'Creado correctamente'
-//     });        
-// })
-
-
 app.use('/app', express.static('public'))   //Toda la info publica que queremos mostrar
-
 
 app.listen(3000)    //Escuchando en el puerto 3000
 console.log('La aplicacion esta escuchando en el puerto 3000');
